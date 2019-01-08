da6596f09650b54988757828111840fcc588bb5c
Shubhankardubey <dubeyshubhankar007@gmail.com>
2019-01-08 17:26:47 +0530

    comment deleted

--- src/modules/mongoose/private/models/accounts.js
+++ src/modules/mongoose/private/models/accounts.js
@@ -13,18 +13,4 @@ const schema = new mongoose.Schema({
   },
 });
 
-// // specify the transform schema option
-// if (!schema.options.toObject) schema.options.toObject = {};
-
-// schema.options.toObject.transform = (doc, ret) => {
-//   // for _it to id transformation
-//   const response = ret;
-//   // delete the original _id
-//   delete response._id;
-//   // add the original _id as id
-//   response.id = doc._id;
-//   // conclude
-//   return response;
-// };
-
 module.exports = mongoose.model('Account', schema);
